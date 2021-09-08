f5f0d13358332b17d420e3ec35887285fa770eaf
Natasha <njohnson533@gmail.com>
2021-09-07 22:28:43 -0400

    added js functionality for hamburger button open/close animation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const hamburgerBtn = document.getElementById("hamburger");
+const hamburgerBtn = document.querySelector(".nav__hamburger");
 const sidebar = document.querySelector(".sidebar");
 
 hamburgerBtn.addEventListener("click", () => {
     sidebar.classList.toggle("show");
-})
+    hamburgerBtn.classList.toggle("open");
+});
