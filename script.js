f332e986eae02b36fcc07e49247b88cbcd3128e3
Natasha <njohnson533@gmail.com>
2021-10-12 17:08:35 -0400

    added event listener to sidebar links

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hamburgerBtn = document.querySelector(".nav__hamburger");
 const sidebar = document.querySelector(".sidebar");
+const sidebarLinks = document.querySelectorAll(".sidebar__link");
+console.log(sidebarLinks);
 
 hamburgerBtn.addEventListener("click", () => {
     sidebar.classList.toggle("show");
@@ -10,4 +12,11 @@ hamburgerBtn.addEventListener("click", () => {
 window.addEventListener("scroll", () => {
     const header = document.querySelector(".header");
     header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+sidebarLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        sidebar.classList.toggle("show");
+        hamburgerBtn.classList.toggle("open");
+    });
 });
